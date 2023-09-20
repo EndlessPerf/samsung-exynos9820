8c50eb4f55cbbfcef57ef5af9740aa13935ecac9
EndlessPerf <142721835+EndlessPerf@users.noreply.github.com>
2023-09-20 11:40:22 +0400

    Update exynos-acme.c

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -317,7 +317,7 @@ static int update_freq(struct exynos_cpufreq_domain *domain,
 		return 0;
 	}
 
-	ret = cpufreq_driver_target(policy, freq, CPUFREQ_RELATION_L);
+	ret = cpufreq_driver_target(policy, freq, CPUFREQ_RELATION_H);
 	cpufreq_cpu_put(policy);
 
 	return ret;
